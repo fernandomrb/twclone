4249e75e556ff14ee19d84104ab4e87e7234c284
fernando <fernan_moises1@hotmail.com>
2017-05-10 17:55:27 -0400

    Style devise views and improves tweets styles

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,15 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   add_flash_types :success, :warning, :danger, :info
+
+  layout :layout_by_resource
+  
+  private
+	  def layout_by_resource
+		  if devise_controller? && resource_name == :user && action_name == "new"
+		    "registration"
+		  else
+		    "application"
+		  end
+		end
 end
